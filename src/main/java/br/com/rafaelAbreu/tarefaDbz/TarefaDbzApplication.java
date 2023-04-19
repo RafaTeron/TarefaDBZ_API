ff3eacba7ee92226172db3eaa3f6ed3a65c1060c
RafaTeron <rafael_sabreu@hotmail.com>
2023-04-19 14:25:56 -0300

    Projeto Criado

---
+++ src/main/java/br/com/rafaelAbreu/tarefaDbz/TarefaDbzApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelAbreu.tarefaDbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaDbzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaDbzApplication.class, args);
+	}
+
+}
