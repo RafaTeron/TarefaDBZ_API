1187fee8a216433b3235606b4f995789f8ded741
RafaTeron <rafael_sabreu@hotmail.com>
2023-08-12 01:09:22 -0300

    Implementando Test UsuarioResources (Part 2)

--- src/test/java/br/com/rafaelAbreu/tarefaDbz/resources/UsuarioResourceTest.java
+++ src/test/java/br/com/rafaelAbreu/tarefaDbz/resources/UsuarioResourceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,9 +23,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.rafaelAbreu.tarefaDbz.builder.UsuarioBuilder;
+import br.com.rafaelAbreu.tarefaDbz.entities.Tarefa;
 import br.com.rafaelAbreu.tarefaDbz.entities.Usuario;
 import br.com.rafaelAbreu.tarefaDbz.entities.enums.Nivel;
 import br.com.rafaelAbreu.tarefaDbz.entities.enums.TipoUsuario;
+import br.com.rafaelAbreu.tarefaDbz.exceptions.ErroTarefaException;
 import br.com.rafaelAbreu.tarefaDbz.repositories.TarefaRepository;
 import br.com.rafaelAbreu.tarefaDbz.repositories.UsuarioRepository;
 import br.com.rafaelAbreu.tarefaDbz.services.UsuarioService;
@@ -134,4 +137,78 @@ public class UsuarioResourceTest {
 				.andExpect(MockMvcResultMatchers.status().isNoContent())
 				.andDo(MockMvcResultHandlers.print());
 	}
+	
+	@Test
+	@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+	void adicionarTarefa() throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setId(2L);
+	    usuario.setNome("Nome do Usuário");
+	    usuarioRepository.save(usuario);
+
+	    Tarefa tarefaFacil = new Tarefa();
+	    tarefaFacil.setNome("Primeira Tarefa");
+	    tarefaFacil.setNivel(Nivel.FACIL);
+	    tarefaRepository.save(tarefaFacil);
+
+	    int opcao = 1;
+
+	    mockMvc.perform(MockMvcRequestBuilders.post("/usuarios/2/adicionarTarefa", usuario.getId())
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .characterEncoding("UTF-8")
+	            .content(Integer.toString(opcao)))
+	            .andExpect(MockMvcResultMatchers.status().isOk())
+	            .andExpect(MockMvcResultMatchers.content().string("Tarefa adicionada com sucesso!"))
+	            .andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+	void adicionarTarefa_UsuarioInexistente() throws Exception {
+	    int opcao = 1; // Defina a opção desejada
+
+	    mockMvc.perform(MockMvcRequestBuilders.post("/usuarios/99/adicionarTarefa")
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .characterEncoding("UTF-8")
+	            .content(Integer.toString(opcao)))
+	            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+	            .andExpect(MockMvcResultMatchers.content().string("Usuário não encontrado!"))
+	            .andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+	void adicionarTarefa_ErroTarefa() throws Exception {
+	    int opcao = -1; // Defina a opção desejada
+	    
+	    Usuario usuario = new Usuario();
+		usuario.setId(2L);
+	    usuario.setNome("Nome do Usuário");
+	    usuarioRepository.save(usuario);
+
+	    Tarefa tarefaFacil = new Tarefa();
+	    tarefaFacil.setNome("Primeira Tarefa");
+	    tarefaFacil.setNivel(Nivel.FACIL);
+	    tarefaFacil.setUsuario(usuario);
+	    
+	    Tarefa tarefaFacil2 = new Tarefa();
+	    tarefaFacil.setNome("Primeira Tarefa");
+	    tarefaFacil.setNivel(Nivel.FACIL);
+	    tarefaRepository.save(tarefaFacil);
+	    tarefaRepository.save(tarefaFacil2);
+
+	    // Simule um cenário onde ocorre um erro relacionado à tarefa
+	    Mockito.doThrow(new ErroTarefaException("Opção inválida! Digite um número inteiro correspondente a uma tarefa disponível."))
+	           .when(usuarioService).adicionarTarefaAoUsuario(Mockito.anyLong(), Mockito.anyInt());
+
+	    mockMvc.perform(MockMvcRequestBuilders.post("/usuarios/2/adicionarTarefa")
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .characterEncoding("UTF-8")
+	            .content(Integer.toString(opcao)))
+	            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+	            .andExpect(MockMvcResultMatchers.content().string("Opção inválida! Digite um número inteiro correspondente a uma tarefa disponível."))
+	            .andDo(MockMvcResultHandlers.print());
+	}
+	
+	
 }
